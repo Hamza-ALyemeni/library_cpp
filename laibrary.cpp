0778b54e18850ad65cce547f81685395668ba6f2
Aloud <Aloud@tad.local>
2023-09-14 16:51:01 +0300

    second commit

--- laibrary.cpp
+++ laibrary.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int MAX_BOOK = 100;
+int added = 0;
 
 struct book{
     int id[MAX_BOOK];
     string name[MAX_BOOK];
     int quantity[MAX_BOOK];
-    int added = 0;
-
+    
     void addbook(){
         cin>>id[added];
         cin>>name[added];
@@ -16,9 +16,18 @@ struct book{
         added++;
     }
 
-}
+    void printbooks()
+    {
+        cout<<id[added];
+        cout<<name[added];
+        cout<<quantity[added];
+    }
+
+};
 
 int main() {
-	
+	book book1;
+    book1.addbook();
+    book1.printbooks();
 	return 0;
 }
